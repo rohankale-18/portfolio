ff6af195a55233a5bc14c36130e2583adce0d9db
Rohan Kale <rohan.kale6123@gmail.com>
2025-10-04 21:59:23 +0530

    Bug fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
   images: {
     unoptimized: true, // important for static export, disables next/image optimization
   },
